b5ba26a7cf4d612dade8922a0af0060602bbdc3f
SewerynKaminski <Seweryn.kaminski@gmail.com>
2023-10-12 19:32:06 +0200

    Adding day8, day9

--- modules/year2021/day8/day8.v
+++ modules/year2021/day8/day8.v
@@ -1,10 +1,145 @@
 module day8
 
-pub fn task1() string {
-	return "1"
+import os
+
+const path = 'modules/year2021/day8/input'
+
+//---------------------------------------------------------------------------//
+struct Display {
+   patterns []string
+   outputs  []string
 }
 
+//---------------------------------------------------------------------------//
+fn load ( file os.File ) []Display {
+    mut data := []Display{};
+
+    mut buf:=[]u8{len:1024}
+    mut l := file.read_bytes_into_newline( mut buf ) or {0}-1
+    for l>0 {
+	//l = file.read_bytes_into_newline( mut buf ) or {0}-1
+	t := buf[0..l].bytestr().split(' | ')
+	mut p := t[0].split(' ')
+	for mut pi in p {
+	   mut pib := pi.bytes()
+	   pib.sort()
+	   pi = pib.bytestr()
+	}
+
+	mut o := t[1].split(' ')	
+	for mut oi in o {
+	   mut oib := oi.bytes()
+	   oib.sort()
+	   oi = oib.bytestr()
+	}
+
+        if p.len == 10 {
+            data << Display{patterns:p, outputs:o}
+	}
+	l = file.read_bytes_into_newline( mut buf ) or {0}-1
+    }
+    return data
+}
+
+//---------------------------------------------------------------------------//
+pub fn task1() string {
+   mut ans := 0
+
+   mut file := os.open_file ( path, 'r' ) or {
+      return "[Error]open"
+   }
+
+   data := load ( file )
+
+   for display in data {
+      for o in display.outputs {
+         if o.len == 2 || o.len == 4 || o.len == 7 || o.len == 3 {
+            ans += 1
+         }
+      }
+   }
+   return ans.str()
+}
+
+//---------------------------------------------------------------------------//
+fn cnt ( a string, b string ) i32 {
+   mut r := 0
+   ab:=a.bytes()
+   bb:=b.bytes()
+   for c in bb {
+       r += u8(c in ab)
+   }
+   return r
+}
+
+//---------------------------------------------------------------------------//
+fn contains ( a string, b string ) bool {
+   ab := a.bytes()
+   for c in b.bytes() {
+        if c !in ab {
+	   return false
+	}
+   }
+
+   return true
+}
+
+//---------------------------------------------------------------------------//
 pub fn task2() string { 
-        return "2"
+   mut ans := 0
+
+   mut file := os.open_file ( path, 'r' ) or {
+      return "[Error]open"
+   }
+
+   data := load ( file )
+
+   for display in data {
+       mut digits := []string{len:10}
+       for s in display.patterns {
+           if s.len == 2 { digits[1] = s }
+           else if s.len == 4 { digits[4] = s }
+           else if s.len == 3 { digits[7] = s }
+           else if s.len == 7 { digits[8] = s }
+       }
+       for s in display.patterns {
+           if s.len == 5 { // 2 lub 3 lub 5
+               // 235 => tylko 3 zawiera 1 lub 7 w calosci
+               //  25 => tylko 5 zawiera >4
+               //   2 => tylko 2
+               if contains ( s, digits[1] ) {
+                   digits[3] = s
+               } else { // 2 lub 5
+                    if cnt ( s, digits[4] ) == 3 {
+                        digits[5] = s
+                    } else {
+                        digits[2] = s
+                    }
+                }
+            } else if s.len == 6 { // 0 lub 6 lub 9
+                if !contains ( s, digits[1] ) {
+                    digits[6] = s
+                } else { // 0 lub 9
+                    if cnt ( s, digits[4] ) == 3 {
+                        digits[0] = s
+                    } else {
+                        digits[9] = s
+                    }
+                }
+            }
+        }
+
+       mut num := 0
+       for o in display.outputs {
+           for i in  0..10 {
+               if digits[i] == o {
+                   num = 10 * num + i
+               }
+           }
+       }
+       ans += num
+   }
+
+   return ans.str()
 }
 
--- modules/year2021/day9/day9.v
+++ modules/year2021/day9/day9.v
@@ -1,10 +1,106 @@
 module day9
 
+import os
+
+const path = 'modules/year2021/day9/input'
+
+fn load( path string ) ![][]u8 {
+   mut file := os.open_file ( path, 'r' )!
+
+   mut data := [][]u8{};
+    /* "2199943210\n"
+       "3987894921\n"
+       "9856789892\n"
+       "8767896789\n"
+       "9899965678\n"*/
+   mut buf :=[]u8{len:128}
+
+   mut l := file.read_bytes_into_newline( mut buf ) or {0}-1
+   for l>0 {
+      data << []u8{}
+      for c in buf {
+         data.last() << ( c - 48 );
+      }
+      l = file.read_bytes_into_newline( mut buf ) or {0}-1
+   }
+   return data
+}
+
+struct Tri{
+   y i32
+   x i32
+   v i32
+}
+
+fn low_points( data [][]u8 ) []Tri {
+    mut ret :=[]Tri{}
+    max_x := data[0].len-1;
+    max_y := data.len-1;
+    for y in 0..data.len {
+        for x in 0..data[0].len {
+            mut a:=false 
+	    mut b:=false 
+            mut c:=false 
+            mut d:=false
+            v := data[y][x];
+            if x < max_x {
+                 if v < data[y][x+1] { a=true }
+            } else { a=true}
+            if x>0 {
+                if v < data[y][x-1] { b=true }
+            } else { b=true}
+            if y < max_y {
+                if v < data[y+1][x] { c=true }
+            } else { c=true}
+            if y > 0 {
+                if v < data[y-1][x] { d=true }
+            } else { d=true }
+
+            if a && b && c && d {
+                ret << Tri{y,x,v}
+            }
+        }
+    }
+    return ret
+}
+
 pub fn task1() string {
-	return "?"
+   mut ans := 0
+
+   data := load ( path )or { return "[Error]Load"}
+
+   lp := low_points( data )
+   for v in lp {
+        ans += v.v + 1;
+   }
+
+   return ans.str()
+}
+
+fn area( mut data [][]u8, x u32, y u32 ) u64 {
+    if y >= data.len ||
+       x >= data.len ||
+       data[y][x]==9 { return 0}
+    data[y][x]=9
+    return 1 + area ( mut data, x-1, y )
+             + area ( mut data, x+1, y )
+             + area ( mut data, x, y+1 )
+             + area ( mut data, x, y-1 )
 }
 
 pub fn task2() string { 
-        return "?"
+   mut data := load( path ) or { return "[Error]Load"}
+
+   lp := low_points( data )
+
+   mut areas := []i32{}
+   for v in lp {
+       areas << area ( mut data, v.x, v.y )
+   }
+
+   areas.sort ( |a,b| a > b )
+   ans := areas[0] * areas[1] * areas[2]
+
+   return ans.str()
 }
 
