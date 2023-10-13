384034a6185e13efe01dd806633755c7f26a7a00
SewerynKaminski <Seweryn.kaminski@gmail.com>
2023-10-13 23:13:29 +0200

    Add day13

--- modules/year2021/day13/day13.v
+++ modules/year2021/day13/day13.v
@@ -2,6 +2,10 @@ module day13
 
 //---------------------------------------------------------------------------//
 import os
+import datatypes
+
+//---------------------------------------------------------------------------//
+const path = 'modules/year2021/day13/input'
 
 //---------------------------------------------------------------------------//
 struct Point {
@@ -9,40 +13,40 @@ mut:
     x int
     y int
 };
+
 //---------------------------------------------------------------------------//
 struct Tci {
 mut:
     c u8
-    i i32
+    v i32
 };
 
+//---------------------------------------------------------------------------//
 struct St{
    p []Point
    f []Tci
 }
 
 //---------------------------------------------------------------------------//
-const path = 'modules/year2021/day12/input'
-
-//---------------------------------------------------------------------------//
 fn load ( ) !St {
-    data := []Point{}
-    fold := []Tci{}
+    mut data := []Point{}
+    mut fold := []Tci{}
 
-    buf []u8{len:1024};
-    
+    mut buf := []u8{len:1024}
+    mut file := os.open_file ( path, 'r' )!
+        
     // punkty
     mut l:= file.read_bytes_into_newline( mut buf )!-1    
-    while l>0 {
+    for l>0 {
         t:=buf[0..l].bytestr().split(',')
-        data << Point{t[0].int(), t[1]int()}
+        data << Point{t[0].int(), t[1].int()}
         l = file.read_bytes_into_newline( mut buf )!-1
     }
     // zlozenia
-    mut l:= file.read_bytes_into_newline( mut buf )!-1    
-    while l>0 {
+    l = file.read_bytes_into_newline( mut buf )!-1    
+    for l>0 {
         c := buf[11]
-        v := buf[13..].int()
+        v := buf[13..].bytestr().int()
         fold << Tci{c, v}
         l = file.read_bytes_into_newline( mut buf )!-1
     }
@@ -52,33 +56,67 @@ fn load ( ) !St {
 
 //---------------------------------------------------------------------------//
 pub fn task1() string {
-    ans := 0
-    data, fold := load ( ) or { return "[Error]"+err.str() }
+    mut data := load ( ) or { return "[Error]"+err.str() }
 
-    a, v = fold[0]
-    for p in data {
-        if a == 'x' {
+    a := data.f[0].c
+    v := data.f[0].v
+    for mut p in data.p {
+        if a == 'x'[0] {
             if p.x > v {
                 p.x = 2 * v - p.x
             }
-        } else {
             if p.y > v {
                 p.y = 2 * v - p.y
             }
         }
     }
 
-    //set<int> paper
-    paper map[int]void
-    for x, y in data {
-        paper.insert ( ( x << 16 ) | y )
+    mut paper := datatypes.Set[int]{}
+    for p in data.p {
+        paper.add( ( p.x << 16 ) | p.y )
     }
-    ans = paper.len
+    ans := paper.size()
 
     return ans.str()
 }
 
+//---------------------------------------------------------------------------//
 pub fn task2() string { 
-        return "?"
+    mut data := load ( ) or { return "[Error]"+err.str() }
+
+    for f in data.f {
+        for mut p in data.p {
+            if f.c == 'x'[0] {
+                if p.x > f.v {
+                    p.x = 2 * f.v - p.x;
+                }
+            } else {
+                if p.y > f.v {
+                    p.y = 2 * f.v - p.y;
+                }
+            }
+        }
+    }
+    mut minx := 0
+    mut maxx := 0
+    mut miny := 0
+    mut maxy := 0
+    for p in data.p {
+        minx = if minx > p.x { p.x } else { minx }
+        maxx = if maxx < p.x { p.x } else { maxx }
+        miny = if miny > p.y { p.y } else { miny }
+        maxy = if maxy < p.y { p.y } else { maxy }
+    }
+
+    mut img := [][]u8{len: maxy + 1, init:[]u8{len: maxx + 1, init: 32 } }
+    for p in data.p {
+        img[p.y][p.x] = '#'[0]
+    }
+    mut ans := " \n"
+    for y in img {
+    	ans += "            " + y.bytestr() + "\n"
+    }
+    return ans
 }
 
+//---------------------------------------------------------------------------//
